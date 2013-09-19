6b2fde5f412bd3de5357d16bf22cee462701152c
oikewll <oikewll@gmail.com>
2013-09-19 17:42:45 +0800

    by shell

--- js/jquery.tab.sh
+++ js/jquery.tab.sh
@@ -1,2 +1,3 @@
 java -jar /Users/goochin/installs/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar --type js --charset utf-8 jquery.tab.js -o jquery.tab-min.js \r
+native2ascii -encoding UTF-8 jquery.tab.js jquery.tab.js;
 native2ascii -encoding UTF-8 jquery.tab-min.js jquery.tab-min.js
